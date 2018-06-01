766a0c6b1e43e57e8a3f0ea2bc62b39f1801bd2c
Jonathan <jtecktips@gmail.com>
2018-06-01 11:26:51 -0700

    Interface Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Person {
+  firstName: string;
+  lastName: string;
+
+  sayHi(): string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    const customer: Person = {
+      firstName: 'My Name ',
+      lastName: 'Jeff',
+      sayHi: () => {
+        console.log(5 + 5);
+        return 'Hi';
+      }
+    }
+    console.log(customer.sayHi());
+
+    const employee: Person = {
+      firstName: 'Dude',
+      lastName: 'Awesome',
+      sayHi: () => {
+        return 'Hello';
+      }
+    }
+    console.log(employee.sayHi());
+  }
 }
